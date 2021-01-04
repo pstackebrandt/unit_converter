edf6fa214a691220bc692b1548ebd98c3f249be5
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-01-04 01:11:48 +0100

    Extend Unit classes
    Add singular to unit
    Change short of unit to list
    Add TemperatureUnit

--- Unit Converter/task/src/converter/Units.kt
+++ Unit Converter/task/src/converter/Units.kt
@@ -1,48 +1,58 @@
 package converter
 
 interface Unit {
+    val singular: String
     val plural: String
-    val short: String
+    val short: List<String>
     val name: String
 
     /** Check ignores case. */
-    fun isEqualMeaning(unitText: String) =
-            when (unitText.toLowerCase()) {
-                name.toLowerCase(), plural.toLowerCase(), short.toLowerCase() -> true
-                else -> false
-            }
+    fun isEqualMeaning(unitText: String): Boolean {
+        val cleanText = unitText.toLowerCase()
 
-    /** Get long unit name in singular or plural. Result depends on [value] and is in lowercase. */
-    fun getLongName(value: Double): String {
-        return if (value == 1.0) {
-            name.toLowerCase()
-        } else {
-            plural.toLowerCase()
+        return when {
+            cleanText == singular -> true
+            cleanText == plural -> true
+            cleanText == name.toLowerCase() -> true
+            short.contains(cleanText) -> true
+            else -> false
         }
     }
+
+    /** Get long unit name in singular or plural. Result depends on [value] and is in lowercase. */
+    fun getLongName(value: Double) =
+            if (value == 1.0) {
+                singular
+            } else {
+                plural
+            }
 }
 
-enum class LengthUnit(override val plural: String, override val short: String) : Unit {
-    Meter("Meters", "m"),
-    Kilometer("Kilometers", "km"),
-    Centimeter("Centimeters", "cm"),
-    Millimeter("Millimeters", "mm"),
-    Mile("Miles", "mi"),
-    Yard("Yards", "yd"),
-    Foot("Feet", "ft"),
-    Inch("Inches", "in")
+enum class LengthUnit(override val singular: String, override val plural: String, override val short: List<String>) : Unit {
+    Meter("meter", "meters", listOf("m")),
+    Kilometer("kilometer", "kilometers", listOf("km")),
+    Centimeter("centimeter", "centimeters", listOf("cm")),
+    Millimeter("millimeter", "millimeters", listOf("mm")),
+    Mile("mile", "miles", listOf("mi")),
+    Yard("yard", "yards", listOf("yd")),
+    Foot("foot", "feet", listOf("ft")),
+    Inch("inch", "inches", listOf("in"))
 }
 
-enum class MassUnit(override val plural: String, override val short: String) : Unit {
-    Gram("grams", "g"),
-    Kilogram("kilograms", "kg"),
-    Milligram("milligrams", "mg"),
-    Pound("pounds", "lb"),
-    Ounce("ounces", "oz"),
+enum class MassUnit(override val singular: String, override val plural: String, override val short: List<String>) : Unit {
+    Gram("gram", "grams", listOf("g")),
+    Kilogram("kilogram", "kilograms", listOf("kg")),
+    Milligram("milligram", "milligrams", listOf("mg")),
+    Pound("pound", "pounds", listOf("lb")),
+    Ounce("ounce", "ounces", listOf("oz")),
 }
 
-
-enum class UnitError(override val plural: String, override val short: String) : Unit {
-    UnknownUnit("unknown units", "???"),
+enum class TemperatureUnit(override val singular: String, override val plural: String, override val short: List<String>) : Unit {
+    Celsius("degree Celsius", "degrees Celsius", listOf("dc", "c")),
+    Kelvin("degree Kelvin", "degrees Kelvin", listOf("dk", "k")),
+    Fahrenheit("degree Fahrenheit", "degrees Fahrenheit", listOf("df", "f")),
 }
 
+enum class UnitError(override val singular: String, override val plural: String, override val short: List<String>) : Unit {
+    UnknownUnit("???", "???", listOf("???")),
+}
